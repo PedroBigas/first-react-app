63bffafc19d2911edbc2671ab018af57341499d1
Pedro Bigas <91469236+PedroBigas@users.noreply.github.com>
2022-08-22 22:12:03 -0300

    Aprendendo sobre estados sem usar hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Counter from './Counter';
 
 function App() {
   return (
-      <Header name='Pedro Bigas' links={['Sobre','Comprar','Contato','Localização']}></Header>  
+    <div>
+      {/* <Header name='Pedro Bigas' links={['Sobre','Comprar','Contato','Localização']}></Header>   */}
+      <Counter count={5}></Counter>
+    </div>
   );
 }
 
